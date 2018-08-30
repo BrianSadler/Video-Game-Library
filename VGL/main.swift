c633ff11aafb71fc5d82f7a052968a142241f1bf
Brian Sadler <briansadler@macbook-pro-3.local>
2018-08-30 10:47:21 -0500

    Initial Commit

---
+++ VGL/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  VGL
+//
+//  Created by Brian Sadler on 8/30/18.
+//  Copyright © 2018 Brian Sadler. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
